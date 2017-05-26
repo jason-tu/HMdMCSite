62a9bac7bfe7006854953ddbea378f2b50176c06
Jason Tu <title.kubo@gmail.com>
2017-05-26 15:00:31 -0400

    Drastically improved layout
    Now correctly displays on more screen sizes

--- js/game.js
+++ js/game.js
@@ -11,7 +11,6 @@ var player = new Mario.Player( [ 0, 0 ] );
 
 function addCanvas() {
     //create the canvas
-    //document.getElementById( "gameOutputTab" ).innerHTML = "";
     canvas = document.createElement( "canvas" );
     canvas.id = "canvasMario";
     ctx = canvas.getContext( '2d' );
